b1846486f721f46d0530439c4501d549230c1bc5
Burak Keskin <burakkeskin09@gmail.com>
2018-09-13 09:02:39 +0300

    Added GuzzleHttp package

--- src/GitHub/Client/Builder/AuthenticationBuilder.php
+++ src/GitHub/Client/Builder/AuthenticationBuilder.php
@@ -1,6 +1,40 @@
 <?php
+namespace Keskinbu\Github\Client\Builder;
+
+use GuzzleHttp\Client;
 
 class AuthenticationBuilder
 {
+    /**
+     * GuzzleHttp Client
+     *
+     * @var Client $client
+     */
+    private $client;
 
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    /**
+     * [test description]
+     * @param  string $test [description]
+     * @return array        [description]
+     */
+    public function test(string $test): array
+    {
+        $test = 2;
+
+        $test =3;
+
+        $test = $test . $test2;
+
+        return [];
+    }
+
+    public function asd()
+    {
+        $this->test('asd');
+    }
 }
